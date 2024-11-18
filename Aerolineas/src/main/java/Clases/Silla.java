6c5a92575d6cb6397f51447273a038cb6ec5a119
Maxi Santucho <maximilianosantucho@outlook.es>
2024-11-18 15:28:52 -0300

    termine clase Silla

--- Aerolineas/src/main/java/Clases/Silla.java
+++ Aerolineas/src/main/java/Clases/Silla.java
@@ -17,6 +17,59 @@ public class Silla {
         this.pasajero = pasajero;
     }
     
+    public String asignarNumero(int fila, int columna) {
+        String[] columnas = {"A", "B", "C", "D"};
+        String[] filas = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        numero = columnas[columna] + filas[fila];
+        return numero;
+    }
+    
+    public String asignasClase(int fila, int columna) {
+        int i = 0;
+        while(i <= fila) {
+            int j = 0;
+            while (j <= columna) {
+                if(i < 3 && j < 4) {
+                    clase = "Ejecutivo";
+                } else {
+                    clase = "Turista";
+                }
+                j++;
+            }
+            i++;
+        }
+        return clase;
+    }
+    
+    public String asignarUbicacion(int columna) {
+        if(columna == 0 || columna == 3) {
+            ubicacion = "Ventana";
+        } else {
+            ubicacion = "Pasillo";
+        }
+        return ubicacion;
+    }
+    
+    public double asignarPrecio() {
+        if(clase.equalsIgnoreCase("ejecutivo") && ubicacion.equalsIgnoreCase("ventana")) {
+            precio = 40000;
+        }
+        if(clase.equalsIgnoreCase("ejecutivo") && ubicacion.equalsIgnoreCase("pasillo")) {
+            precio = 30000;
+        }
+        if(clase.equalsIgnoreCase("turista") && ubicacion.equalsIgnoreCase("ventana")) {
+            precio = 25000;
+        }
+        if(clase.equalsIgnoreCase("turista") && ubicacion.equalsIgnoreCase("pasillo")) {
+            precio = 20000;
+        }
+        return precio;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+    
     public int solicitarPasajero() {
         return pasajero.getIdentificacion();
     }
