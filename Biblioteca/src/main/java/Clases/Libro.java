c284d9a9b97e404d2376285905d4b8960f19324b
Maxi Santucho <maximilianosantucho@outlook.es>
2024-11-18 22:26:23 -0300

    se agrego prestarLibro

--- Biblioteca/src/main/java/Clases/Libro.java
+++ Biblioteca/src/main/java/Clases/Libro.java
@@ -3,13 +3,34 @@ package Clases;
 
 public class Libro {
     
-    private String titulo;
     private String isbn;
+    private String titulo;
     private boolean prestado;
 
-    public Libro(String titulo, String isbn) {
-        this.titulo = titulo;
+    public Libro(String isbn, String titulo) {
         this.isbn = isbn;
+        this.titulo = titulo;
+    }
+    
+    public void cambiarEstado() {
+        prestado = true; 
+    }
+
+    public boolean isPrestado() {
+        return prestado;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public String toString() {
+        String estado = "Disponible";
+        if(prestado) {
+            estado = "No disponible";
+        }
+        return "Libro{" + "isbn=" + isbn + ", titulo=" + titulo + ", estado=" + estado + '}';
     }
     
 }
